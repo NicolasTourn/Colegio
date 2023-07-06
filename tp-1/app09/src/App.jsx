04e91f20312cd2f5005927ed5a40d8b1f3d4e736
NicolasTourn <132491386+NicolasTourn@users.noreply.github.com>
2023-07-05 21:12:02 -0300

    Add files via upload

---
+++ tp-1/app09/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import { useState } from "react";
+import CoordenadaFlecha from './CoordenadaFlecha';
+
+function App() {
+
+  const [visible, setVisible] = useState(true)
+
+  function ocultar() {
+    setVisible(false)
+  }
+
+  return (
+    <div>
+      {visible ? <CoordenadaFlecha /> : <p>Se oculto la coordenada</p>}
+      <button onClick={ocultar}>Ocultar</button>
+    </div>
+  );
+}
+
+export default App
